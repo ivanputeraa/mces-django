f9562b1e92e19c336cedae266ce685199b2c0694
Ivan Putera Pratama <ivanputeraa@gmail.com>
2018-12-27 19:32:03 +0800

    Add minimum yield_rate value in trend chart

--- trends/views.py
+++ trends/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Max, Min
 
 from .forms import *
 
@@ -35,6 +36,13 @@ def get_machine_trends_and_maintenance(request):
             .filter(machine=machine, start_period__range=(start_date, end_date), end_period__range=(start_date, end_date), analyze_type=report_type) \
             .order_by('end_period')
 
+        min_yield_rate = Machine_Yield_Rate_History.objects.values_list('yield_rate')\
+            .filter(machine=machine,
+                    start_period__range=(start_date, end_date),
+                    end_period__range=(start_date, end_date),
+                    analyze_type=report_type)\
+            .aggregate(Min('yield_rate'))
+
         machine_maintenance = Maintenance_History.objects \
             .values('check_in_time', 'employee_id', 'operation_class', 'description', 'solution', 'reason') \
             .filter(machine=machine, check_in_time__range=(start_date, end_date)) \
@@ -63,6 +71,7 @@ def get_machine_trends_and_maintenance(request):
 
         result = json.dumps({
             'yield_rate': list(machine_yield_rates),
+            'min_yield_rate': min_yield_rate['yield_rate__min'] - 0.01,
             'maintenance_history': maintenance_occurrence_dict,
         })
 
@@ -72,7 +81,7 @@ def get_machine_trends_and_maintenance(request):
 def machine_autocomplete(request):
     if request.is_ajax():
         machine = request.GET.get('search', None)
-        queryset = Machine_Yield_Rate_History.objects.filter(machine__startswith=machine)
+        queryset = Machine_Yield_Rate_History.objects.filter(machine__icontains=machine)
 
         # Because MySQL db backend doesn't support DISTINCT ON syntax,
         # so manually filter duplicate machine names from the queryset
